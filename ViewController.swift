0a3f99080d37db53e94f4db5638e7b91a1fb9707
Jie Wu <jiewu97@gmail.com>
2019-02-17 11:02:33 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Invealth
+//
+//  Created by Jie Wu on 2/16/19.
+//  Copyright © 2019 Jie Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .blue
+        self.navigationItem.title = title
+        self.navigationController?.navigationBar.barTintColor = .white
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: makeBackButton())
+    }
+    func makeBackButton() -> UIButton {
+        let backButtonImage = UIImage(named: "backbutton.png")?.withRenderingMode(.alwaysTemplate)
+        let backButton = UIButton(type: .custom)
+        backButton.setImage(backButtonImage, for: .normal)
+        backButton.tintColor = .blue
+        backButton.setTitle("  Back", for: .normal)
+        backButton.setTitleColor(.blue, for: .normal)
+        backButton.addTarget(self, action: #selector(self.backButtonPressed), for: .touchUpInside)
+        return backButton
+    }
+    
+    @objc func backButtonPressed() {
+        dismiss(animated: true, completion: nil)
+        //        navigationController?.popViewController(animated: true)
+    }
+}
+
